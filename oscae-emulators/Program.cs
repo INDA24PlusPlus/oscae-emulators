a3742187614439d7906f3761f968599155d3b787
Oscar Eriksson <oscareriksson2004@hotmail.com>
2024-11-01 14:44:37 +0100

    Add project files.

---
+++ oscae-emulators/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace oscae_emulators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+        }
+    }
+}
